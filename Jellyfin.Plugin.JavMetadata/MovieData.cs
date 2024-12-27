e62300fec5642ae39920ec193a2b10173f85cc04
Sleepmuffin <Sleepmuffin@protonmail.com>
2024-12-27 12:35:43 -0300

    fix

--- Jellyfin.Plugin.JavMetadata/MovieData.cs
+++ Jellyfin.Plugin.JavMetadata/MovieData.cs
@@ -7,52 +7,52 @@ public class MovieData
     public List<object> authors { get; set; }
     public List<CategoriesData> categories { get; set; }
     public string? comment_en { get; set; }
-    public string content_id { get; set; }
+    public string? content_id { get; set; }
     public List<object> directors { get; set; }
-    public string dvd_id { get; set; }
+    public string? dvd_id { get; set; }
     public List<GalleryData> gallery { get; set; }
     public List<object> histrions { get; set; }
-    public string jacket_full_url { get; set; }
-    public string jacket_thumb_url { get; set; }
-    public int label_id { get; set; }
-    public string label_name_en { get; set; }
-    public string label_name_ja { get; set; }
-    public int maker_id { get; set; }
-    public string maker_name_en { get; set; }
-    public string maker_name_ja { get; set; }
-    public string release_date { get; set; }
-    public short runtime_mins { get; set; }
-    public string sample_url { get; set; }
-    public int series_id { get; set; }
-    public string series_name_en { get; set; }
+    public string? jacket_full_url { get; set; }
+    public string? jacket_thumb_url { get; set; }
+    public int? label_id { get; set; }
+    public string? label_name_en { get; set; }
+    public string? label_name_ja { get; set; }
+    public int? maker_id { get; set; }
+    public string? maker_name_en { get; set; }
+    public string? maker_name_ja { get; set; }
+    public string? release_date { get; set; }
+    public short? runtime_mins { get; set; }
+    public string? sample_url { get; set; }
+    public int? series_id { get; set; }
+    public string? series_name_en { get; set; }
     public bool series_name_en_is_machine_translation { get; set; }
-    public string series_name_ja { get; set; }
-    public string service_code { get; set; }
-    public int site_id { get; set; }
-    public string title_en { get; set; }
+    public string? series_name_ja { get; set; }
+    public string? service_code { get; set; }
+    public int? site_id { get; set; }
+    public string? title_en { get; set; }
     public bool title_en_is_machine_translation { get; set; }
-    public string title_ja { get; set; }
+    public string? title_ja { get; set; }
 }
 
 public class ActressData
 {
     public long id { get; set; }
-    public string image_url { get; set; }
-    public string name_kana { get; set; }
-    public string name_kanji { get; set; }
-    public string name_romaji { get; set; }
+    public string? image_url { get; set; }
+    public string? name_kana { get; set; }
+    public string? name_kanji { get; set; }
+    public string? name_romaji { get; set; }
 }
 
 public class CategoriesData
 {
     public long id { get; set; }
-    public string name_en { get; set; }
+    public string? name_en { get; set; }
     public bool name_en_is_machine_translation { get; set; }
-    public string name_ja { get; set; }
+    public string? name_ja { get; set; }
 }
 
 public class GalleryData
 {
-    public string image_full { get; set; }
-    public string image_thumb { get; set; }
+    public string? image_full { get; set; }
+    public string? image_thumb { get; set; }
 }
