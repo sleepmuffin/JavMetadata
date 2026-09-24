21f41cc0b7f9f4836af31ca17fb8c216aee72122
Sleepmuffin <Sleepmuffin@protonmail.com>
2026-09-24 17:50:11 -0300

    Bumping versions

--- Jellyfin.Plugin.JavMetadata/Providers/MetadataServe/MetadataServeExternalId.cs
+++ Jellyfin.Plugin.JavMetadata/Providers/MetadataServe/MetadataServeExternalId.cs
@@ -1,3 +1,4 @@
+using MediaBrowser.Controller.Entities;
 using MediaBrowser.Controller.Entities.Movies;
 using MediaBrowser.Controller.Providers;
 using MediaBrowser.Model.Entities;
@@ -14,14 +15,28 @@ public class MetadataServeExternalId : IExternalId
     public string Key => "MetadataServe";
 
     /// <inheritdoc />
-    public string UrlFormatString => "http://192.168.0.100:8456/videos/{0}";
-
-    /// <inheritdoc />
     public ExternalIdMediaType? Type => ExternalIdMediaType.Movie;
 
     /// <inheritdoc />
     public bool Supports(IHasProviderIds item)
     {
         return item is Movie;
+    }
+}
+
+public class MetadataServeExternalUrlProvider : IExternalUrlProvider
+{
+    /// <inheritdoc />
+    public string Name => "MetadataServe";
+
+    /// <inheritdoc />
+    public IEnumerable<string> GetExternalUrls(BaseItem item)
+    {
+        if (item is Movie
+            && item.TryGetProviderId("MetadataServe", out var id)
+            && !string.IsNullOrEmpty(id))
+        {
+            yield return $"http://192.168.0.100:8456/videos/{id}";
+        }
     }
 }
